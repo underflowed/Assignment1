b894c0d2a824f702e74a8ae9c45e0c447c821871
Jose Oliver <40325724+underflowed@users.noreply.github.com>
2022-10-21 17:19:28 +0000

    Changed layout and added new visuals around the website in accordance with Assignment 3 guidlines

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
-function validate(){
-  const TAKENUSERNAMES = new Array("Harry","Elizabeth","Shana");
-  let enteredUsername = document.getElementById("username").value;
-  let pw = document.getElementById("password").value;
-  let pwConfirm = document.getElementById("confirmPassword").value;
-  if(pw != pwConfirm) {
+function validate() { // Function used for input validation on page 3
+  const TAKENUSERNAMES = new Array("Harry", "Elizabeth", "Shana");
+  let enteredUsername = document.getElementById("username").value; //Accesses entered username in DOM
+  let enteredPw = document.getElementById("password").value;
+  let enteredpwConfirm = document.getElementById("confirmPassword").value;
+  if (enteredPww != enteredpwConfirm) {
     alert("The passwords do not match!");
-    return false;
+    return false; // returning false here so form does not submit
   }
-  if(TAKENUSERNAMES.includes(enteredUsername)){
+  if (TAKENUSERNAMES.includes(enteredUsername)) {
     alert("That username is taken!");
     return false;
   }
+}
+
+let canvas = document.getElementById("mycanvas");
+
+if (canvas.getContext) {
+  let ctx = canvas.getContext('2d');
+  ctx.font = "20px Helvitica";
+  ctx.textBaseline = 'top';
+  ctx.textAlign = "start";
+  ctx.fillText("Top o' the mornin to ya!", 50, 50);
+  ctx.strokeText("Top o' the mornin to ya!", 50, 50);
+} else {
+  alert("Canvas not supported on this browser");
 }
